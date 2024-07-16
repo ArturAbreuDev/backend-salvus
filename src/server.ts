5dd1fcb412d114a5a7787534e0f0f5d5b42eb573
Artur Damaceno Abreu <arturabreu110@gmail.com>
2024-07-15 21:06:22 -0300

    arrumar importacao do server.ts e ajustar erro interno

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import express, { Express, Request, Response, NextFunction } from 'express';
 import createProduct from "./routes/create-product";
 import getProducts from "./routes/get-products";
 import getProduct from "./routes/get-product";
@@ -5,8 +6,7 @@ import deleteProduct from "./routes/delete-product";
 import updateProduct from "./routes/update-product";
 import { env } from './env'
 
-const express = require("express");
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
 
@@ -16,6 +16,10 @@ app.use(getProduct);
 app.use(deleteProduct);
 app.use(updateProduct);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
 
 app.listen(env.PORT, () => {
   console.log(`server running at ${env.PORT}`);
